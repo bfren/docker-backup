3462175fb592feb3a50c911c9ed99ca08bba1fb7
bfren <bf@bfren.dev>
2022-12-15 12:32:24 +0000

    Adding workspace files

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+IMAGE=`cat VERSION`
+
+docker buildx build \
+    --build-arg BF_IMAGE=backup \
+    --build-arg BF_VERSION=${IMAGE} \
+    -f Dockerfile \
+    -t backup-dev \
+    . \
+    && \
+    docker run -it -e PASSPHRASE=fred -e BACKUP_RCLONE_STORAGE=fred backup-dev sh
